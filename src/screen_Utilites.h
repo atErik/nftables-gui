5b08f97496e1b358b9b7a226141752b2532d32bd
atErik <aterik@outlook.com>
2020-07-22 03:52:45 -0700

    only non-english portions converted into english

--- src/screen_Utilites.h
+++ src/screen_Utilites.h
@@ -8,7 +8,8 @@
 #define WIDTH 30
 #define HEIGHT 10 
 void createChain();
-int print_menu(WINDOW *menu_win, int highlight, char *choices[], int n_choices); //metodo para el el menu, 
+// method for the menu, // metodo para el el menu,
+int print_menu(WINDOW *menu_win, int highlight, char *choices[], int n_choices);
 void createRule();
 void createTable();
 void createTableIP();
@@ -28,15 +29,18 @@ void createRule(){
 	mvprintw(0,0,"Please select the type of rule");
 	refresh();
 	char *choices[]={"accept","drop","reject","redirect"};
-	int n_choices = sizeof(choices) / sizeof(char *); //numero de opciones
+	int n_choices = sizeof(choices) / sizeof(char *); //numero de opciones //number of options
 	WINDOW *menu_win;
-	menu_win = newwin(HEIGHT, WIDTH, starty, startx); // metodo para crear un menu, es de ncurses
-	keypad(menu_win, TRUE); //obtenemos datos del teclado
+	// method to create a menu, it's from ncurses // metodo para crear un menu, es de ncurses
+	menu_win = newwin(HEIGHT, WIDTH, starty, startx);
+	keypad(menu_win, TRUE); //obtenemos datos del teclado // we get data from the keyboard
 	refresh();
 	int salida=print_menu(menu_win, 1,choices,n_choices);
-	//seleccionamos la salida y vemos que hacemos para crear la tabla
+	// seleccionamos la salida y vemos que hacemos para crear la tabla
+	// we select the output and see what we do to create the table
 	switch(salida){
 		case 1: //accept, podemos aprovectar la misma window en la que estamos
+			//accept, we can deprovecte the same window we're in
 			
 			break;
 		case 2: //drop
@@ -61,13 +65,15 @@ void createTable(){
 	mvprintw(0,0,"Please select the family");
 	refresh();
 	char *choices[]={"ip","ip6","arp","nat"};
-	int n_choices = sizeof(choices) / sizeof(char *); //numero de opciones
+	int n_choices = sizeof(choices) / sizeof(char *); //numero de opciones //number of options
 	WINDOW *menu_win;
-	menu_win = newwin(HEIGHT, WIDTH, starty, startx); // metodo para crear un menu, es de ncurses
-	keypad(menu_win, TRUE); //obtenemos datos del teclado
+	// method to create a menu, it's from ncurses // metodo para crear un menu, es de ncurses
+	menu_win = newwin(HEIGHT, WIDTH, starty, startx);
+	keypad(menu_win, TRUE); //obtenemos datos del teclado // we get data from the keyboard
 	refresh();
 	int salida=print_menu(menu_win, 1,choices,n_choices);
 	//seleccionamos la salida y vemos que hacemos para crear la tabla
+	//we select the output and see what we do to create the table
 	switch(salida){
 		case 1: //ip
 			createTableIP();
@@ -140,7 +146,7 @@ void createTableIP(){
 		}
 	}
 
-	/* Un post form and free the memory */
+	/* // Un post form and free the memory // A post form and free the memory */
 	unpost_form(my_form);
 	free_form(my_form);
 	
@@ -179,7 +185,8 @@ void createChain(){
     
     switch(salida){
         case 1: //input
-            //debemos de llamar a las tablas que hay existentes
+            // debemos de llamar a las tablas que hay existentes
+	    // we should call the tables that are existing
         	 
         	 
         	 
@@ -216,6 +223,7 @@ void createChain(){
 int print_menu(WINDOW *menu_win, int highlight, char *choices[], int n_choices)
 {
     //tarea: modificar metodo para que solo necesite el array de las opciones, que el solo imprima y detecte el menu
+    //task: modify method so that you only need the array of options, that the only one prints and detects the menu
 	int x, y, i;	
 
 	int choice = 0;
@@ -223,8 +231,8 @@ int print_menu(WINDOW *menu_win, int highlight, char *choices[], int n_choices)
         
         
 	
-	while(1) //infinito para vovler a el desde los ficheros de funciones
-	{	
+	while(1)	//infinito para vovler a el desde los ficheros de funciones
+	{		//infinite for vovler to the from function files
 
 	
 
@@ -247,13 +255,18 @@ int print_menu(WINDOW *menu_win, int highlight, char *choices[], int n_choices)
 
 		
 		
-		        c = wgetch(menu_win); // obtenemos las pulsaciones del teclado
+			// we get the keystrokes // obtenemos las pulsaciones del teclado
+		        c = wgetch(menu_win);
 		        switch(c)
-		        {	case KEY_UP: //flecha arriba
-				        if(highlight == 1) // comprobamos que si highlight es 1 (primera posicion) lo dejamos igual (es decirlo, lo seleccionamos como high)
-					        highlight = n_choices; // lo dejamos a 1 (n_choises era 0)
+		        {	case KEY_UP: //flecha arriba //arrow up
+			 		// we check that if highlight is 1 (first position) we leave it the same (i.e. we select it as high)
+			 		// comprobamos que si highlight es 1 (primera posicion) lo dejamos igual (es decirlo, lo seleccionamos como high)
+				        if(highlight == 1)
+						// we left it at 1 (n_choises was 0) // lo dejamos a 1 (n_choises era 0)
+					        highlight = n_choices;
 				        else
-					        --highlight; // si no es uno, subimos de posicion
+						// if it's not one, we're up in position // si no es uno, subimos de posicion
+					        --highlight;
 				        break;
 			        case KEY_DOWN:
 				        if(highlight == n_choices)
@@ -265,16 +278,18 @@ int print_menu(WINDOW *menu_win, int highlight, char *choices[], int n_choices)
 				
 				        break;
 			        case 10:
-				        choice = highlight; // caso intro, pasa a elegir la opcion
+			 		// case intro, it happens to choose the option // caso intro, pasa a elegir la opcion
+				        choice = highlight;
 				        break;
 			        default:
-				        mvprintw(24, 0, "Charcter pressed is = %3d Hopefully it can be printed as '%c'", c, c);
+				        mvprintw(24, 0, "Character pressed is = %3d Hopefully it can be printed as '%c'", c, c);
 				        refresh();
 				        break;
 		        }
 		
 
-		        if(choice != 0)	/*se sale del loop infinito, por lo cual procedemos a hacer segun la choise elegida */
+		        if(choice != 0)	/* se sale del loop infinito, por lo cual procedemos a hacer segun la choise elegida.
+					   goes out of the infinite loop, so we proceed to do according to the chosen choice. */
 			        break;
 		        else
 			        continue;
@@ -283,7 +298,9 @@ int print_menu(WINDOW *menu_win, int highlight, char *choices[], int n_choices)
 	mvprintw(23, 0, "You chose choice %d with choice string %s\n", choice, choices[choice - 1]);
 	
 	clrtoeol();
-	refresh();// aqui en funcion de la choice hacemos las cosas necesarias
+	//here in function of the choice we do the necessary things
+	//aqui en funcion de la choice hacemos las cosas necesarias
+	refresh();
 	//createChain();
 	
 	return choice;
